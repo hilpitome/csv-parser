8d51bc51baa04ed0abfc16d87b69088e85195d10
hilpitome <hilpitome@gmail.com>
2019-09-18 22:31:50 +0300

    init filetoobj converter

---
+++ src/CsvToObjConverter.java
@@ -0,0 +1,22 @@
+public class CsvToObjConverter {
+    private String filePath = "";
+    public CsvToObjConverter(){
+
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void readFile(){
+
+    }
+
+    public void convertFileToObjs(){
+
+    }
+}
