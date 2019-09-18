1a3c402b08f92999fb1868b60ef082daa8c52f1b
hilpitome <hilpitome@gmail.com>
2019-09-19 01:02:35 +0300

    keys for object wrapper hashmap

--- src/CsvToObjConverter.java
+++ src/CsvToObjConverter.java
@@ -1,18 +1,23 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class CsvToObjConverter {
     private String filePath = "";
     private BufferedReader csvReader = null;
+    private String delimiter = ",";
     public CsvToObjConverter(){}
 
     public String getFilePath() {
         return filePath;
     }
 
-    public void initFileReader(String filePath) {
+    public void setDelimiter(String delimiter){
+        this.delimiter = delimiter;
+    }
+
+    public void setFilePath(String filePath) {
         this.filePath = filePath;
 
     }
@@ -22,9 +27,15 @@ public class CsvToObjConverter {
         System.out.println(this.filePath);
         try {
             csvReader = new BufferedReader(new FileReader(this.filePath));
+            int index = 0;
             while ((row = csvReader.readLine()) != null) {
 //                String[] data = row.split(",");
-                System.out.println(row);
+                if(index==0){
+                    // create header row
+                    convertHeadersToUnderscoreKeys(row);
+
+                }
+                index++;
             }
             csvReader.close();
         } catch (IOException e) {
@@ -34,5 +45,28 @@ public class CsvToObjConverter {
 
     public void convertFileToObjs(){
 
+    }
+
+    /**
+     * @param row header row of the csv not yet split by the delimeter
+     * @return String[] of strings with underscores
+     */
+    public String[] convertHeadersToUnderscoreKeys(String row){
+
+        String[] headerRowArr = row.split(delimiter);
+        System.out.println(Arrays.toString(headerRowArr));
+        String[] headerRowArrPascaled = new String[headerRowArr.length];
+        for(int i = 0; i < headerRowArr.length; i++ ){
+
+            headerRowArrPascaled[i] = insertUnderscores(headerRowArr[i]);
+
+        }
+        System.out.println(Arrays.toString(headerRowArrPascaled));
+        return  headerRowArrPascaled;
+
+    }
+
+    private String insertUnderscores(String text) {
+       return String.join("_", text.toLowerCase().split(" "));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         CsvToObjConverter converter = new CsvToObjConverter();
-        converter.initFileReader("C2ImportFamRelSample.csv");
+        converter.setFilePath("C2ImportCalEventSample.csv");
         converter.readFile();
 
     }
